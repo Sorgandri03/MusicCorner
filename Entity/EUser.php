db3866c47d47589d54ed354a50c2bef855624ae4
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-04-26 11:37:57 +0200

    Added attribute to EUser.php ddddd

---
+++ Entity/EUser.php
@@ -0,0 +1,28 @@
+<?php
+class EUser {
+    private String $email;
+
+    private String $password;
+    
+    public function __construct(string $email, string $password) {
+        $this->email = $email;
+        $this->password = $password;
+    }
+    
+    public function getEmail(): string {
+        return $this->email;
+    }
+    
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void {
+        $this->password = $password;
+    }
+
+}
