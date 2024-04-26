ce43b568afc399399b560f076fd796ce4c76bf09
Andrea Sorge <andrea.sorge@student.univaq.it>
2024-04-26 13:21:57 +0200

    Completed EShoppingCart

--- Entity/EShoppingCart.php
+++ Entity/EShoppingCart.php
@@ -1,14 +1,26 @@
-use EArticleDescription;
 <?php
 namespace Entity;
 class EShoppingCart {
     private string $id;
-    private array $articles;
+    private array $articles = array();
     private ECustomer $customer;
-    public function __construct(string $id, array $articles, ECustomer $customer) {
+    public function __construct(string $id, ECustomer $customer) {
         $this->id = $id;
-        $this->articles = $articles;
         $this->customer = $customer;
+    }
+
+    public function getId(): string {
+        return $this->id;
+    }
+    public function getArticles(): array {
+        return $this->articles;
+    }
+    public function getCustomer(): ECustomer {
+        return $this->customer;
+    }
+
+    public function addArticle(EArticleDescription $article): void {
+        $this->articles[] = $article;
     }
    
 }
