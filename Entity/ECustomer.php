60522a66609294f44689af7a311d7931b3b21808
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-04-26 12:35:43 +0200

    added ECustomer.php

--- Entity/ECustomer.php
+++ Entity/ECustomer.php
@@ -1,19 +1,46 @@
 <?php
 
-
 class ECustomer extends EUser {
-    private String $username;
+    private  $username;
 
     private String $customerId;
     
-    private int $suspensionTime;
+    private int $suspensionTime=0;
 
-    private array $addresses;
+    private array $addresses = array();
     
     private array $creditCards = array();
     
-    private function addCreditCard() {
-        $creditCards[] = new ECreditCard();
+    public function addCreditCard(string $number, string $expiration_date, string $cvv, string $owner_name, string $billing_address) {
+        $creditCards[] = new ECreditCard( $number,  $expiration_date,  $cvv,  $owner_name,  $billing_address);
     }
+
+    public function addAddress(string $street, string $city, int $cap, string $receiver_name) {
+        $creditCards[] = new EAddress( $street,  $city,  $cap,  $receiver_name);
+    }
+     
+    //metodo per il ban
+
+    public function __construct(string $username, string $customerId) {
+        $this->username = $username;
+        $this->customerId = $customerId;
+    }
+
+    public function getUsername(): string {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void {
+        $this->username = $username;
+    }
+
+    public function getCustomerId(): string {
+        return $this->customerId;
+    }
+
+    public function setCustomerId(string $customerId): void {
+        $this->customerId = $customerId;
+    }
+
 
 }
