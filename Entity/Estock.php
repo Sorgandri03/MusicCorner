be282b71e85df0dc0b5a694ee142f0b913555167
Andrea Sorge <andrea.sorge@student.univaq.it>
2024-04-26 12:33:07 +0200

    Added stock

---
+++ Entity/Estock.php
@@ -0,0 +1,25 @@
+<?php
+
+class Stock {
+    private EArticleDescription $Article;
+    private int $quantity;
+    private float $price;
+
+    public function __construct(EArticleDescription $Article, int $quantity, float $price) {
+        $this->Article = $Article;
+        $this->quantity = $quantity;
+        $this->price = $price;
+    }
+    
+    public function getArticle(): EArticleDescription {
+        return $this->Article;
+    }
+
+    public function getQuantity(): int {
+        return $this->quantity;
+    }
+
+    public function getPrice(): float {
+        return $this->price;
+    }
+}
