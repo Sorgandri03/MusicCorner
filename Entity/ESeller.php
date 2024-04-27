7590d6723ceaeb5fdf32e7c720f11e369051467c
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-04-27 17:38:28 +0200

    ddd

--- Entity/ESeller.php
+++ Entity/ESeller.php
@@ -3,21 +3,25 @@
 class ESeller extends EUser {    
     private String $shopName;
     
-    private float $shopRating;
+    private float $shopRating=0;
 
     private array $catalogue = array();
     
-    public function __construct(string $email, string $password, float $shopRating, string $shopName) {
+    public function __construct(string $email, string $password, string $shopName) {
         parent::__construct($email, $password);
         $this->shopName = $shopName;
-        $this->shopRating=$shopRating;
     }
+    
     public function addStock(float $price, int $quantity, EArticleDescription $article) {
         $catalogue[] = new EStock(  $article,  $quantity,  $price);
     }
     public function getShopName(): string {
         return $this->shopName;
     }
+    public function setRating(): void {
+      //DA FARE
+    }
+
 
     public function setShopName(string $shopName): void {
         $this->shopName = $shopName;
