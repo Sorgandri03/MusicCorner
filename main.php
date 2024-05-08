575aac5c3cdbcfd9e271112e74174e3b5e9754c4
Sorgandri03 <sorgandri03@gmail.com>
2024-05-08 10:09:02 +0200

    New index

--- main.php
+++ main.php
@@ -1,21 +1,24 @@
 <?php
 
-spl_autoload_register(function ($className) { 
-    $filePath = __DIR__ . '/Entity/' . $className . '.php'; 
-    if (file_exists($filePath)) 
-    { require_once $filePath; } });
+class Main {
 
+    public function __construct() {
+        $z = new EArticleDescription("1", "2", "3", "4", Format::CD);
 
-$z = new EArticleDescription("1", "2", "3", "4", Format::CD);
+        $B = new ESeller("pippo.coca@dipre.it", "mussolini", "bellaRga");
 
-$B = new ESeller("pippo.coca@dipre.it", "mussolini", "bellaRga");
+        $a = new EStock($z, "10","10");
 
-$a = new EStock($z, "10","10");
+        $B->addStock($z, "10","10");
 
-$B->addStock($z, "10","10");
+        $a = $B->getCatalogue();
+        foreach ($a as $key) {
+            echo $key;
+        }
 
-$a = $B->getCatalogue();
-foreach ($a as $key) {
-    echo $key;
+    }
+
 }
+
+
 
