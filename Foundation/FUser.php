a2bc3624fb1988e6f81c9c48599987b3d4f5fcc8
Gianmarco Cavuoto <gianmarco.cavuoto@student.univaq.it>
2024-05-10 11:59:46 +0200

    new file:   Foundation/FAdmin.php

---
+++ Foundation/FUser.php
@@ -0,0 +1,47 @@
+<?php
+class FUser{
+    private static $table = "User";
+    private static $value = "(:email, :password)";
+    public static function getValue(): string {
+        return self::$value;
+    }
+    public static function getTable(): string {
+        return self::$table;
+    }
+    public static function getKey(): string {
+        return self::$key;
+    }
+
+    public static function bind($stmt, $user){
+        $stmt->bindValue(':email', $user->getEmail(), PDO::PARAM_STR);
+        $stmt->bindValue(':password', $user->getPassword(), PDO::PARAM_STR);
+    }
+        
+
+    public static function saveObj($obj){
+        $saveArticle = FDB::getInstance()->saveObject(self::class, $obj);
+        if($saveArticle !== null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static function getObj($email){
+        $result = FDB::getInstance()->retriveObj(self::getTable(), self::getKey(), $email);
+        if(count($result) > 0){
+            $obj = self::createObj($result);
+            return $card;
+        }else{
+            return null;
+        }
+
+    }
+
+    public static function createObj($result){
+        $entity2 = FUser::getObj($result[0]['entity2']);
+        $obj = new EEntity();
+        return $obj;
+    }
+
+}
