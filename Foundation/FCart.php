bb00cbbf1694fbf918966d9661775c0fc2899618
Andrea Sorge <andrea.sorge@student.univaq.it>
2024-05-10 12:00:17 +0200

    New Cart

--- Foundation/FCart.php
+++ Foundation/FCart.php
@@ -1,35 +1,52 @@
 <?php
 
 class FShoppingCart{
-    private static $table = "shoppingcart";
-    private static $value = "(NULL, :customer, :createdAt)";
-    private static $key = "ID";
+    private static $table = "Cart";
+    private static $value = "(NULL, :customer)";
+    private static $key = "id";
     public static function getValue(): string {
         return self::$value;
     }
     public static function getTable(): string {
         return self::$table;
     }
-    public function getKey(): string {
+    public static function getKey(): string {
         return self::$key;
     }
 
     public static function bind($stmt, $shoppingCart){
         $stmt->bindValue(':customer', $shoppingCart->getCustomer()->getEmail(), PDO::PARAM_STR);
-        $stmt->bindValue(':createdAt', $shoppingCart->get(), PDO::PARAM_STR);
     }
 
     public static function saveObj($obj){
         $saveArticle = FDB::getInstance()->saveObject(self::class, $obj);
         if($saveArticle !== null){
+            $obj->setId($saveArticle);
             return true;
         }else{
             return false;
         }
     }
 
-    public static function getItems(){
-        
+    public static function getObj($id){
+        $result = FDB::getInstance()->retriveObj(self::getTable(), self::getKey(), $id);
+        if(count($result) > 0){
+            $obj = self::createObj($result);
+            return $obj;
+        }else{
+            return null;
+        }
+
+    }
+
+    public static function createObj($result){
+        $customer = FCustomer::getObj($result[0]['customer']);
+        $obj = new ECart($customer);
+        return $obj;
+    }
+
+    public static function getCartItems() : array {
+        return [];
     }
     
     
