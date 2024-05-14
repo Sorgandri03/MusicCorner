ee768d00a7f4d1b8fe12f501318f729c0ceeada0
Gianmarco Cavuoto <gianmarco.cavuoto@student.univaq.it>
2024-05-14 12:33:41 +0200

    FUser.php - PHP User Class

--- Foundation/FUser.php
+++ Foundation/FUser.php
@@ -3,6 +3,7 @@ class FUser{
     private static $table = "User";
     private static $value = "(:email, :password)";
     private static $key = "email";
+    private static $updatequery = "email = :email, password = :password";
     public static function getValue(): string {
         return self::$value;
     }
@@ -12,34 +13,56 @@ class FUser{
     public static function getKey(): string {
         return self::$key;
     }
+    public static function getUpdateQuery(): string {
+        return self::$updatequery;
+    }
 
     public static function bind($stmt, $user){
         $stmt->bindValue(':email', $user->getEmail(), PDO::PARAM_STR);
         $stmt->bindValue(':password', $user->getPassword(), PDO::PARAM_STR);
     }
         
-
-    public static function saveObj($obj){
-        $saveArticle = FDB::getInstance()->saveObject(self::class, $obj);
+  //C
+    public static function createObject($obj){
+        $saveArticle = FDB::getInstance()->create(self::class, $obj);
         if($saveArticle !== null){
             return true;
         }else{
             return false;
         }
     }
-
-    public static function getObj($email){
-        $result = FDB::getInstance()->retriveObj(self::getTable(), self::getKey(), $email);
+   //R
+    public static function retrieveObject($email){
+        $result = FDB::getInstance()->retrieve(self::getTable(), self::getKey(), $email);
         if(count($result) > 0){
-            $obj = self::createObj($result);
+            $obj = self::createEntity($result);
             return $obj;
         }else{
             return null;
         }
 
     }
+    //U
+    public static function updateObject($obj){
+        $updateArticle = FDB::getInstance()->update(self::class, $obj);
+        if($updateArticle !== null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    //D
+    public static function deleteObject($obj){
+        $deleteArticle = FDB::getInstance()->delete(self::class, $obj);
+        if($deleteArticle !== null){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
-    public static function createObj($result){
+
+    public static function createEntity($result){
         $obj = new EUser($result[0]['email'],$result[0]['password']);
         return $obj;
     }
@@ -47,6 +70,6 @@ class FUser{
 
     public static function saveCustomer($customer){
         $user = new EUser($customer->getEmail(), $customer->getPassword());
-        return self::saveObj($user);
+        return self::updateObject($user);
     }
 }
