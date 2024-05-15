9cd1865786afbba1c8e2228008a4806e284d6a0a
Gianmarco Cavuoto <gianmarco.cavuoto@student.univaq.it>
2024-05-16 00:04:26 +0200

    prova main 2

--- main.php
+++ main.php
@@ -4,7 +4,7 @@ class Main {
     public function __construct() {
         $cart = FPersistentManager::getInstance()->retrieveObj(ECart::class, 1);
         $array = $cart->getCartItems();
-        echo count($array);
+        echo "\r\n".count($array);
     }
 }
 
