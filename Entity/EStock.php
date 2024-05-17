1997772678658189bb6552fcfe3e2fa3d46f23de
Andrea Sorge <andrea.sorge@student.univaq.it>
2024-05-17 09:51:51 +0200

    Changed to uppercase

---
+++ Entity/EStock.php
@@ -0,0 +1,39 @@
+<?php
+
+class EStock {
+    private int $id;
+    private float $price;
+    private int $quantity;
+    private string $article;
+    private string $seller;
+    public function __construct(string $article, int $quantity, float $price, string $seller) {
+        $this->article = $article;
+        $this->quantity = $quantity;
+        $this->price = $price;
+        $this->seller = $seller;
+    }
+    
+    
+    public function getArticle(): string {
+        return $this->article;
+    }
+
+    public function getQuantity(): int {
+        return $this->quantity;
+    }
+
+    public function getPrice(): float {
+        return $this->price;
+    }
+  
+    public function getId(): int {
+        return $this->id;
+    }
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+    public function getSeller(): string {
+        return $this->seller;
+    }
+    
+}
