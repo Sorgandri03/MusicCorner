139225b06924fb5272dc9e7feec6649ecc540989
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-05-31 17:45:25 +0200

    login.tpl

--- Smarty/configs/f123f58d13bccb888cb040bd52408472408f16cd_0.file_login.tpl.php
+++ Smarty/configs/f123f58d13bccb888cb040bd52408472408f16cd_0.file_login.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 5.1.0, created on 2024-05-31 17:36:57
+/* Smarty version 5.1.0, created on 2024-05-31 17:44:06
   from 'file:login.tpl' */
 
 /* @var \Smarty\Template $_smarty_tpl */
 if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   'version' => '5.1.0',
-  'unifunc' => 'content_6659ee9928d342_28095882',
+  'unifunc' => 'content_6659f0466dcd49_02725162',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'f123f58d13bccb888cb040bd52408472408f16cd' => 
     array (
       0 => 'login.tpl',
-      1 => 1717169814,
+      1 => 1717170243,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   array (
   ),
 ))) {
-function content_6659ee9928d342_28095882 (\Smarty\Template $_smarty_tpl) {
+function content_6659f0466dcd49_02725162 (\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = 'C:\\xampp\\htdocs\\MusicCorner\\Smarty\\templates';
 ?><!DOCTYPE html>
 <html lang="en">
@@ -64,145 +64,118 @@ $_smarty_current_dir = 'C:\\xampp\\htdocs\\MusicCorner\\Smarty\\templates';
 
     </head>
 	<body>
-		<div class=".main-content">
-            <!-- HEADER -->
-            <header>
-                <!-- MAIN HEADER -->
-                <div id="header">
-                    <!-- container -->
-                    <div class="container">
-                        <!-- row -->
-                        <div class="row">
-                            <!-- LOGO -->
-                            <div class="col-md-3">
-                                <div class="header-logo">
-                                    <a href="/MusicCorner/" class="logo">
-                                        <img src="/MusicCorner/Smarty/templates/img/hhh.png" alt="">
-                                    </a>
-                                </div>
-                            </div>
-                            <!-- /LOGO -->
+		<!-- HEADER -->
+		<header>
+		    <!-- MAIN HEADER -->
+			<div id="header">
+				<!-- container -->
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<!-- LOGO -->
+						<div class="col-md-3">
+							<div class="header-logo">
+								<a href="/MusicCorner/" class="logo">
+									<img src="/MusicCorner/Smarty/templates/img/hhh.png" alt="">
+								</a>
+							</div>
+						</div>
+						<!-- /LOGO -->																						
+					</div>
+					<!-- row -->
+				</div>
+				<!-- container -->
+			</div>
+			<!-- /MAIN HEADER -->
+		</header>
+		<!-- /HEADER -->
 
-                            
-                            
-                            
+		
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
 
-                            
-                            
-                            
-                        </div>
-                        <!-- row -->
-                    </div>
-                    <!-- container -->
-                </div>
-                <!-- /MAIN HEADER -->
-            </header>
-            <!-- /HEADER -->
-        
-            <!-- SECTION -->
-            <div class="section">
-                <!-- container -->
-                <div class="container">
-                    <!-- row -->
-                    <div class="row">
+					<div class="col-md-7">
+						<!-- Billing Details -->
+						<div class="login-form">
+							<div class="section-title">
+								<h3 class="title">Login</h3>
+							</div>
+							<div class="form-group">
+								<input class="input" type="text" name="first-name" placeholder="Email">
+							</div>
+							<div class="form-group">
+								<input class="input" type="text" name="last-name" placeholder="Password">
+                            </div>																				
+						</div>
+						<!-- /Billing Details -->																																			
+					</div>										
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+						
+		<!-- FOOTER -->
+		<footer id="footer">
+			<!-- top footer -->
+			
+			<!-- /top footer -->
 
-                        <div class="col-md-7">
-                            <!-- Billing Details -->
-                            <div class="login-form">
-                                <div class="section-title">
-                                    <h3 class="title">Login</h3>
-                                </div>
-                                <div class="form-group">
-                                    <input class="input" type="text" name="first-name" placeholder="Email">
-                                </div>
-                                <div class="form-group">
-                                    <input class="input" type="text" name="last-name" placeholder="Password">
-                                </div>
-                                
-                                
-                                
-                                
-                                
-                                
-                                
-                            </div>
-                            <!-- /Billing Details -->
-
-                            
-                            
-                            
-
-                            
-                            
-                            
-                        </div>
-
-                        
-                        
-                        
-                    </div>
-                    <!-- /row -->
-                </div>
-                <!-- /container -->
-            </div>
-            <!-- /SECTION -->
-        </div>
-    
-            <!-- FOOTER -->
-            <footer id="footer">
-                
-                <!-- bottom footer -->
-                <div id="bottom-footer" class="section">
-                    <div class="container">
-                        <!-- row -->
-                        <div class="row">
-                            <div class="col-md-12 text-center">
-                                <ul class="footer-payments">
-                                    <li><a href="#"><i class="fa fa-cc-visa"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-credit-card"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-cc-paypal"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-cc-mastercard"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-cc-discover"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-cc-amex"></i></a></li>
-                                </ul>
-                                <span class="copyright">
-                                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                    Copyright ©<?php echo '<script'; ?>
+			<!-- bottom footer -->
+			<div id="bottom-footer" class="section">
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<ul class="footer-payments">
+								<li><a href="#"><i class="fa fa-cc-visa"></i></a></li>
+								<li><a href="#"><i class="fa fa-credit-card"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-paypal"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-mastercard"></i></a></li>
+							</ul>
+							<span class="copyright">
+								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+								Copyright ©<?php echo '<script'; ?>
 >document.write(new Date().getFullYear());<?php echo '</script'; ?>
 >2024 All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                </span>
-                            </div>
-                        </div>
-                            <!-- /row -->
-                    </div>
-                    <!-- /container -->
-                </div>
-                <!-- /bottom footer -->
-            </footer>
-            <!-- /FOOTER -->
+							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+							</span>
+						</div>
+					</div>
+						<!-- /row -->
+				</div>
+				<!-- /container -->
+			</div>
+			<!-- /bottom footer -->
+		</footer>
+		<!-- /FOOTER -->
 
-            <!-- jQuery Plugins -->
-            <?php echo '<script'; ?>
+		<!-- jQuery Plugins -->
+		<?php echo '<script'; ?>
  src="js/jquery.min.js"><?php echo '</script'; ?>
 >
-            <?php echo '<script'; ?>
+		<?php echo '<script'; ?>
  src="js/bootstrap.min.js"><?php echo '</script'; ?>
 >
-            <?php echo '<script'; ?>
+		<?php echo '<script'; ?>
  src="js/slick.min.js"><?php echo '</script'; ?>
 >
-            <?php echo '<script'; ?>
+		<?php echo '<script'; ?>
  src="js/nouislider.min.js"><?php echo '</script'; ?>
 >
-            <?php echo '<script'; ?>
+		<?php echo '<script'; ?>
  src="js/jquery.zoom.min.js"><?php echo '</script'; ?>
 >
-            <?php echo '<script'; ?>
+		<?php echo '<script'; ?>
  src="js/main.js"><?php echo '</script'; ?>
 >
 
-        
+	
 
-    </body></html><?php }
+</body></html><?php }
 }
