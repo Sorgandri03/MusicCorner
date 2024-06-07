cbd220048d076406c9ca40c5b8722337159dab12
Gianmarco Cavuoto <gianmarco.cavuoto@student.univaq.it>
2024-06-07 16:43:30 +0200

    new files

--- View/VUser.php
+++ View/VUser.php
@@ -61,5 +61,25 @@ class VUser{
         $this->smarty->assign('regErr',false);
         $this->smarty->display('login.tpl');
     }
+/**
+     * @throws SmartyException
+     */
+    public function showUserDashboard()
+    {
+        if(USession::getInstance()->isSetSessionElement('customer'))
+            $this->smarty->assign('username',USession::getInstance()->getSessionElement('customer'));
+        else
+            $this->smarty->assign('username',USession::getInstance()->getSessionElement('seller'));
+
+        
+    }
+   public function showCustomerDashboard()
+    {
+        $this->smarty->display('customerDashboard.tpl');
+    }
+    public function showSellerDashboard()
+    {
+        $this->smarty->display('sellerDashboard.tpl');
+    }
 
 }
