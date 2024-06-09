5ac72fce25c3cbf079432a0ea3a7c04e75cc0b2a
Gianmarco Cavuoto <gianmarco.cavuoto@student.univaq.it>
2024-06-09 18:59:53 +0200

    jjjjj

--- View/VUser.php
+++ View/VUser.php
@@ -44,7 +44,6 @@ class VUser{
      */
     public function showUserDashboard(){
         if(USession::getInstance()->isSetSessionElement('customer')){
-            $this->smarty->assign('username',USession::getInstance()->getSessionElement('customer')->getUsername());
             $this->smarty->display('customer.tpl');
         }
         elseif(USession::getInstance()->isSetSessionElement('seller')){
