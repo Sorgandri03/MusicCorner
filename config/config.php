1c0c074e346b8db69e7bad40e16a74e0888aab77
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-06-27 18:00:34 +0200

    ee

--- config/config.php
+++ config/config.php
@@ -8,6 +8,7 @@ define('DB_USER', 'MusicCorner_wayvowelso');
 define('DB_PASS', 'c687f12dcd3469af32fd5e63e9c152f2d69fa6bb');
 define('SQL_FILE_PATH', 'musiccorner.sql');
 */
+
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'MusicCorner_wayvowelso');
 define('DB_USER', 'root');
