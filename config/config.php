1003857954599bd80e3b8fe0517807aaeea3fcbe
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-07-01 16:12:35 +0200

    fixes

--- config/config.php
+++ config/config.php
@@ -3,23 +3,23 @@
  * This file contains the configuration of the database and the global arrays
  */
 
-/*
+
 define('DB_HOST', '5jg.h.filess.io:3305');
 define('DB_NAME', 'MusicCorner_wayvowelso');
 define('DB_USER', 'MusicCorner_wayvowelso');
 define('DB_PASS', 'c687f12dcd3469af32fd5e63e9c152f2d69fa6bb');
 define('SQL_FILE_PATH', 'musiccorner.sql');
-*/
+
 
 /**
  * Database configuration 
- */
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'MusicCorner_wayvowelso');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('SQL_FILE_PATH', 'musiccorner.sql');
 
+*define('DB_HOST', '127.0.0.1');
+*define('DB_NAME', 'MusicCorner_wayvowelso');
+*define('DB_USER', 'root');
+*define('DB_PASS', '');
+*define('SQL_FILE_PATH', 'musiccorner.sql');
+*/
 /**
  * The formats of the music sold
  */
